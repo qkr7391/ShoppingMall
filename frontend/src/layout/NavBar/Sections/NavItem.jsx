da4c1cfd0d3bfa141a23dd46b74899d304fb7859
Saemi Park <saemipqrk7391@gmail.com>
2024-06-24 16:52:28 -0400

    Adding source code for the Shop app

--- frontend/src/layout/NavBar/Sections/NavItem.jsx
+++ frontend/src/layout/NavBar/Sections/NavItem.jsx
@@ -3,17 +3,28 @@ import PropTypes from "prop-types";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { logoutUser } from "../../../store/thunkFunction";
-// import { AiOutlineShoppingCart } from "react-icons/ai";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 // import { useRouteId } from "react-router/dist/lib/hooks";
 
 const routes = [
 	{ to: "/login", name: "Sign In", auth: false },
 	{ to: "/register", name: "Sign Up", auth: false },
 	{ to: "", name: "Logout", auth: true },
+
+	{ to: "/product/upload", name: "Upload", auth: true },
+	{
+		to: "/user/cart",
+		name: "Cart",
+		auth: true,
+		icon: <AiOutlineShoppingCart style={{ fontSize: "1.4rem" }} />,
+	},
+	{ to: "/orders", name: "Orders", auth: true },
 ];
 
 const NavItem = ({ mobile }) => {
 	const isAuth = useSelector((state) => state.user?.isAuth);
+
+	const cart = useSelector((state) => state.user?.userData?.cart);
 
 	const dispatch = useDispatch();
 	const navigate = useNavigate();
@@ -34,7 +45,7 @@ const NavItem = ({ mobile }) => {
 				mobile && "flex-col bg-gray-900 h-full"
 			} items-center`}
 		>
-			{routes.map(({ to, name, auth }) => {
+			{routes.map(({ to, name, auth, icon }) => {
 				if (isAuth !== auth) return null;
 
 				if (name === "Logout") {
@@ -46,11 +57,21 @@ const NavItem = ({ mobile }) => {
 							<Link onClick={handleLogout}>{name}</Link>
 						</li>
 					);
-				}
-				//  else if (icon) {
-
-				//  }
-				else {
+				} else if (icon) {
+					return (
+						<li
+							className="relative py-2 text-center border-b-4 cursor-pointer"
+							key={name}
+						>
+							<Link to={to}>
+								{icon}
+								<span className="absolute top-0 inline-flex items-center justify-center w-4 h-4 text-xs font-bold text-white border-2 border-white rounded-full -right-3">
+									{cart ? cart.length : 0}
+								</span>
+							</Link>
+						</li>
+					);
+				} else {
 					return (
 						<li
 							key={name}
